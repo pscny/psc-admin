b0b002ced3f33e0102aef1edc49727d802d1ac7f
Calin Gribble <calingribble@gmail.com>
2013-01-03 19:16:18 -0500

    Initializing our app with rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PscAdmin::Application.config.secret_token = 'c2059ed6598ed020070d3002b88a55857bfa62e4eb1399c8e149e874d5317e53339dc5a300ad7312a853b74833ef7fbf168e9f72a160a7e48bd5898227eea2fa'
