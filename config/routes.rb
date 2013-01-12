603b1a8bf75dfaed2ba74fb8b26c5e10bdae6e34
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-01-12 00:20:44 -0500

    admins making admins

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ PscAdmin::Application.routes.draw do
   match 'sign_out', :controller => :sessions, :action => :destroy, :as => :sign_out
 
   resources :sessions, :only => [ :create ]
+  resources :admins
 
   root :to => 'dashboard#show'
 end
--- test/factories/admins.rb
+++ test/factories/admins.rb
@@ -1,8 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+FactoryGirl.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
 
 FactoryGirl.define do
   factory :admin do
-    email    'admin@example.com'
+    email    { generate(:email) }
     password 'sekret'
   end
 end
