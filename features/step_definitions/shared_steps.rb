fe89541c32a49af785dea2c3a8de4dd74a73f217
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-01-14 15:49:28 -0500

    Admin sees an error when their username or password is wrong
    closes #8
    
    [Fixes #42441193]

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -1,8 +1,15 @@
-Given /^I am logged in$/ do
-  admin = FactoryGirl.create(:admin)
+def login(admin)
   visit('/')
   click_link('Login')
   fill_in('Email',    :with => admin.email)
   fill_in('Password', :with => admin.password)
   click_button('Sign In')
+end
+
+Given /^I am logged in$/ do
+  login(FactoryGirl.create(:admin))
+end
+
+Given /^I am logged in as:$/ do |table|
+  login(FactoryGirl.create(:admin, table.hashes.first))
 end
