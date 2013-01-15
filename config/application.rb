89b86fa009c98d0c79adb6276af5c09e39d5f46b
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-01-14 21:09:23 -0500

    another config options

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module PscAdmin
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    #config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
     #config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
